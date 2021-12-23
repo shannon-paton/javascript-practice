b04e231a3acbe1922e6be94bb489279b6cc1540a
shannon-paton <79596670+shannon-paton@users.noreply.github.com>
2021-12-23 15:30:16 -0500

    Add files via upload

---
+++ jumping-game/js/script.js
@@ -0,0 +1,30 @@
+var character = document.getElementById("character");
+
+var block = document.getElementById("block");
+
+function jump () {
+	if(character.classList != "animate"){
+		character.classList.add("animate");
+	}
+	setTimeout(function(){
+		character.classList.remove("animate");
+	},500);
+}
+
+var checkLose = setInterval(function(){
+
+	//check if character and block are hitting each other
+
+	var characterTop =
+	parseInt (window.getComputedStyle(character).getPropertyValue("top"));
+
+	var blockLeft =
+	parseInt (window.getComputedStyle(block).getPropertyValue("left"));
+
+	if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+		block.style.animation = "none";
+		block.style.display = "none";
+		alert("you lose!")
+	}
+
+},10);
